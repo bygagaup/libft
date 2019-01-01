ef4759085dd192892b2b2c5cf9b1051e6902ac12
Frost giant Schiller <fschille@e4r1p3.21-school.ru>
2019-01-01 19:38:38 +0300

    libft(2)

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fschille <fschille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 16:58:15 by fschille          #+#    #+#             */
-/*   Updated: 2018/12/09 14:47:59 by fschille         ###   ########.fr       */
+/*   Updated: 2019/01/01 19:30:54 by fschille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,24 @@ t_list		*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*start;
 	t_list	*new_list;
-	t_list	*temp;
 
-	if (lst == NULL || f == NULL)
+	if (lst == NULL || !f)
 		return (NULL);
-	new_list = ft_lstnew(lst->content, lst->content_size);
-	if (new_list == NULL)
+	start = (t_list*)malloc(sizeof(t_list));
+	if (start == NULL)
 		return (NULL);
-	new_list = f(new_list);
-	start = new_list;
+	start = f(lst);
+	new_list = start;
 	while (lst->next != NULL)
 	{
 		lst = lst->next;
-		temp = ft_lstnew(lst->content, lst->content_size);
-		if (temp == NULL)
+		new_list->next = (t_list*)malloc(sizeof(t_list));
+		if (new_list->next == NULL)
 		{
 			ft_lstdel(&start, &ft_del);
 			return (NULL);
 		}
-		temp = f(temp);
-		new_list->next = temp;
+		new_list->next = f(lst);
 		new_list = new_list->next;
 	}
 	return (start);
